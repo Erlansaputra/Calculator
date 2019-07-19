58fa8aa7be05b72c255c6b7297f330edb50fd1ea
Erlansaputra <Erlansaputra2201@gmail.com>
2019-07-19 10:27:28 +0700

    Calculator

---
+++ script.js
@@ -0,0 +1,11 @@
+//function hitung() {
+    //var x = document.form.angka1.value;
+    //var y = document.form.angka2.value;
+    //document.getElementById("nilai").innerHTML = parseInt(x)+parseInt(y);
+}
+
+function hitung() {
+    var angka1 = document.form.angka1.value;
+    var angka2 = document.form.angka2.value;
+    document.getElementById("nilai") = eval(angka1,angka2)
+} 
